a841eef2329de04531e5ea54ad87d3c7cd235f10
qqhann <qiu.gits@gmail.com>
2020-02-11 19:58:13 +0900

    Update: AC d

--- dp_contest/d.py
+++ dp_contest/d.py
@@ -7,17 +7,19 @@ for _ in range(N):
     W.append(_w)
     V.append(_v)
 
-# dp = [[0] * (WW+1) for _ in range(N+1)]
-dp = [[0] * (N+1) for _ in range(WW+1)]
-for j in range(WW+1):
-    # アイテムの数だけ探索する
-    for i in range(1, N+1):
-        w = W[i-1]
-        v = V[i-1]
-        if j+w > WW:
-            continue
-        dp[j+w][i] = max(dp[j+w][i-1], max(dp[j][:i]) + v)
+dp = [[0] * (WW+1) for _ in range(N+1)]
+for i in range(1, N+1):
+    # i := 1...N
+    for j in range(1, WW+1):
+        # j := 0...WW
+        
+        wi = W[i-1]
+        vi = V[i-1]
+        if j-wi < 0:
+            dp[i][j] = dp[i-1][j]
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-wi] + vi)
 
 # import numpy as np
 # print(np.array(dp))
-print(max(dp[WW]))
+print(dp[N][WW])
