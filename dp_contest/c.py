85090cefa15cba3e9bacfdf7c2b4fd4434833067
qqhann <qiu.gits@gmail.com>
2020-02-11 16:30:12 +0900

    Refactor: PyPy3 doesn't import numpy

--- dp_contest/c.py
+++ dp_contest/c.py
@@ -1,14 +1,13 @@
-import numpy as np
 N = int(input())
-ABC = np.zeros([N, 3])
+ABC = []
 for i in range(N):
-    ABC[i] = list(map(int, input().split()))
+    ABC.append(list(map(int, input().split())))
 
-dp = np.zeros([N, 3])
+dp = [[0]*3 for _ in range(N)]
 dp[0] = ABC[0]
 for i in range(N-1):
-    dp[i+1, 0] = max(dp[i, 1], dp[i, 2]) + ABC[i+1, 0]
-    dp[i+1, 1] = max(dp[i, 0], dp[i, 2]) + ABC[i+1, 1]
-    dp[i+1, 2] = max(dp[i, 0], dp[i, 1]) + ABC[i+1, 2]
+    dp[i+1][0] = max(dp[i][1], dp[i][2]) + ABC[i+1][0]
+    dp[i+1][1] = max(dp[i][0], dp[i][2]) + ABC[i+1][1]
+    dp[i+1][2] = max(dp[i][0], dp[i][1]) + ABC[i+1][2]
 
-print(int(max(dp[N-1])))
+print(max(dp[N-1]))
